888fd139aebfefdfe0fc62f7d7e806553b1e1b78
ScevolaDandera <rashim900@gmail.com>
2022-02-02 18:15:58 +0000

    not much progress

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class MainScene extends Scene3D {
         this.clock = new THREE.Clock();
         this.delta = 0;
         this.modelReady = false;
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.25, 1000);
     }
 
     preload() {
@@ -26,24 +27,20 @@ class MainScene extends Scene3D {
 
     create() {
         const { ground, light, sky } = this.warpSpeed('-camera');
-      //     this.physics.debug.enable();
-           const axesHelper = new THREE.AxesHelper( 5 );
-            this.scene.add( axesHelper );
-        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.25, 1000 );
-        this.scene.add( this.camera );
-        this.camera.position.set(0, 2, -2 );
-		this.camera.lookAt( new THREE.Vector3( 2, 4, 2 ) );
 
-        const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-        hemiLight.position.set( 0, 20, 0 );
-        this.scene.add( hemiLight );
+        //     this.physics.debug.enable();
+        const axesHelper = new THREE.AxesHelper(5);
+        this.scene.add(axesHelper);
 
-        const dirLight = new THREE.DirectionalLight( 0xffffff );
-        dirLight.position.set( 0, 20, 10 );
-        this.scene.add( dirLight );
 
-        console.log(this.scene);
 
+        const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+        hemiLight.position.set(0, 20, 0);
+        this.scene.add(hemiLight);
+
+        const dirLight = new THREE.DirectionalLight(0xffffff);
+        dirLight.position.set(0, 20, 10);
+        this.scene.add(dirLight);
 
 
         this.mixer = new THREE.AnimationMixer(this.scene);
@@ -53,27 +50,44 @@ class MainScene extends Scene3D {
             this.action = this.mixer.clipAction(fbx[1].animations[0]);
             this.action.play();
             this.erika = fbx[0];
+            this.erika.position.set(0, 0, 0);
             this.scene.add(this.erika);
+
+            //add camera to the scene
+            this.erika.add(this.camera);
+            this.camera.position.set( -1, 200, -150 );
+            const currentPlayerPosition = new THREE.Vector3().copy(this.erika.position);
+            currentPlayerPosition.setY(currentPlayerPosition.y + 1.5);
+            // currentPlayerPosition.setY(1000);
+            // currentPlayerPosition.setZ(1000);
+            console.log(currentPlayerPosition)
+            this.camera.lookAt(currentPlayerPosition);
+
             this.erikaspeed = 500;
             this.physics.add.existing(this.erika, {
                 shape: 'convexMesh',
-                mass: 0.1,
+                mass: 10,
                 collisionFlags: 1,
             });
-
-
-
             if (this.erika.body) {
                 const loadingdiv = document.getElementById('loading');
                 loadingdiv.style.display = 'none';
+
+
+
+
             }
+
         });
 
 
 
 
-        document.onkeydown = this.keyboardControls.bind(this);
-        document.onkeyup = this.stopMovingPlayer.bind(this);
+        this.setEventListeners();
+
+
+
+
 
 
     }
@@ -82,6 +96,11 @@ class MainScene extends Scene3D {
         this.action.play();
     }
 
+    setEventListeners() {
+        document.onkeydown = this.keyboardControls.bind(this);
+        document.onkeyup = this.stopMovingPlayer.bind(this);
+        window.addEventListener('resize', this.onWindowResize.bind(this), false);
+    }
 
     keyboardControls(e) {
         this.stopMoving = false;
@@ -89,16 +108,17 @@ class MainScene extends Scene3D {
         this.erika.body.setCollisionFlags(2);
         switch (e.key) {
             case 'w':
-                this.erika.position.z -= 0.5;
+                this.erika.position.z += 0.1;
                 break;
             case 'a':
-                this.erika.position.x -= 0.5;
+                this.erika.rotateY(0.1);
+                console.log(this.erika.rotation.y)
                 break;
             case 's':
-                this.erika.position.z += 0.5;
+                this.erika.position.z -= 0.1;
                 break;
             case 'd':
-                this.erika.position.x += 0.5;
+                this.erika.rotateY(-0.1);
                 break;
             case ' ':
                 if (this.erika.position.y < 2) {
@@ -108,6 +128,7 @@ class MainScene extends Scene3D {
         }
         //   this.haveSomeFun();
         this.erika.body.needUpdate = true;
+        this.cameraUpdate();
 
         //once updated, set collision flag to dynamic
         this.erika.body.once.update(() => {
@@ -118,27 +139,31 @@ class MainScene extends Scene3D {
 
     }
 
+    onWindowResize() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+    }
+
     cameraUpdate() {
-        //this.camera
-        // follow camera with player
-        // this.camera.position.x = this.erika.position.x;
-        // this.camera.position.y = this.erika.position.y + 2;
-        // this.camera.position.z = this.erika.position.z;
-        this.camera.lookAt(this.erika.position);
+        const currentPlayerPosition = new THREE.Vector3().copy(this.erika.position);
+        const targetCameraPosition = new THREE.Vector3(currentPlayerPosition.x, currentPlayerPosition.y + 1.5, currentPlayerPosition.z);
+        // const idealCameraLookAt = new THREE.Vector3(this.camera.position).lerp(targetCameraPosition, 0.1);
+        // this.camera.lookAt(idealCameraLookAt);
 
     }
 
     update() {
         if (this.mixer) {
             this.delta = this.clock.getDelta();
-         //   this.mixer.update(this.delta * this.erikaspeed);
+            //   this.mixer.update(this.delta * this.erikaspeed);
             // this.erika.position.z += this.delta * this.erikaspeed * 1.5;
-            if(this.camera) {
-                this.cameraUpdate();
-            }
+            // if (this.camera) {
+            //     this.cameraUpdate();
+            // }
         }
-      
-      
+
+
 
 
     }
